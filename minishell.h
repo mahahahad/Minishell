85265a5c8f5e21dd9da6af4a9212e9fad3e61617
Ali Danish <dannyboy810110@gmail.com>
2024-04-04 00:57:32 +0400

    Added the minimal aspect of 2 of the builtins.

--- minishell.h
+++ minishell.h
@@ -3,10 +3,19 @@
 
 # define PROMPT "minishell> "
 
-#include <readline/readline.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
+# include <readline/readline.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <stdbool.h>
+
+typedef struct s_environment t_env;
+
+struct s_environment
+{
+	char	*env_var;
+	t_env	*next;
+};
 
 #endif
