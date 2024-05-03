5d54238c11f4761d412b9d6b234788f93228dc16
mahahahad <mahad.abd2003@gmail.com>
2024-05-03 23:52:54 +0400

    Updated tokens to have a type

--- includes/minishell.h
+++ includes/minishell.h
@@ -20,9 +20,29 @@
 # include <termios.h>
 # include <unistd.h>
 
+enum	e_token_types
+{
+	PIPE,
+	LESS,
+	DBL_LESS,
+	GREAT,
+	DBL_GREAT,
+	WORD
+};
+
+typedef struct s_token
+{
+	int	type;
+	char	*content;
+} t_token;
+
 typedef struct s_minishell
 {
-	char	**tokens;
+	t_token	*tokens;
 }			t_minishell;
+
+typedef struct s_cmd
+{
+} t_cmd;
 
 #endif
